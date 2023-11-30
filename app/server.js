2357aa6d9274f93eeab912f40cefaed2dc22aa2d
Mageacallll <bryan1mageacal@gmail.com>
2023-11-30 11:36:00 -0800

    fixed the env file bug by deleting it lmao

--- app/server.js
+++ app/server.js
@@ -4,10 +4,9 @@ const appController = require('./appController');
 // Load environment variables from .env file
 // Ensure your .env file has the required database credentials.
 const loadEnvFile = require('./utils/envUtil');
-const envVariables = loadEnvFile('./.env');
 
 const app = express();
-const PORT = envVariables.PORT || 65534;  // Adjust the PORT if needed (e.g., if you encounter a "port already occupied" error)
+const PORT = 3000 || 65534;  // Adjust the PORT if needed (e.g., if you encounter a "port already occupied" error)
 
 // Middleware setup
 app.use(express.static('public'));  // Serve static files from the 'public' directory
